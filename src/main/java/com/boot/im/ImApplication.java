7600e0a0041dd8cd4c9dab7fa4dff6fd4e23bf1c
Lilg <a917968474@gmail.com>
2023-08-18 17:43:37 +0800

    init

---
+++ src/main/java/com/boot/im/ImApplication.java
@@ -0,0 +1,18 @@
+package com.boot.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author sky
+ * @Date 2023/8/18 17:35
+ * @Description TODO
+ */
+
+@SpringBootApplication
+public class ImApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ImApplication.class, args);
+	}
+}
+
